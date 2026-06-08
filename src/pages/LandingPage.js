82d917046097fbf60a3748f5718372c9c7f67591
Eduardo0500 <anchundia2000.l@gmail.com>
2026-06-08 08:40:37 -0500

    despliegue

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -34,7 +34,7 @@ export function LandingPage() {
         <div class="hero-content fade-in">
           <div class="hero-icon">🚌</div>
           <h1 class="hero-title">
-            Sistema de Carritos
+            Gstiona tu transporte universitario con el sistema de carritos
             <span class="gradient-text">ULEAM</span>
           </h1>
           <p class="hero-subtitle">
